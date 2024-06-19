ece608428b6f3a80c4765eabe8df7c85da17c108
Brian Nelson <briannelson@Brians-MacBook-Air.local>
2024-06-19 19:24:50 -0400

    removed heic

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,6 @@ use crate::conversions::{
     convert_png_to_webp,
     convert_webp_to_png,
     convert_pdf_to_image,
-    convert_heic_to_jpeg,
-    convert_heic_to_png,
-    convert_heic_to_webp,
 };
 use open;
 
@@ -31,9 +28,6 @@ enum ConversionType {
     PdfToJpeg,
     PdfToPng,
     PdfToWebp,
-    HeicToJpeg, // New conversion type
-    HeicToPng,  // New conversion type
-    HeicToWebp, // New conversion type
 }
 
 struct MyApp {
@@ -68,7 +62,6 @@ impl MyApp {
                 "webp" => vec![ConversionType::WebpToJpeg, ConversionType::WebpToPng],
                 "png" => vec![ConversionType::PngToJpeg, ConversionType::PngToWebp],
                 "pdf" => vec![ConversionType::PdfToJpeg, ConversionType::PdfToPng, ConversionType:: PdfToWebp],
-                "heic" => vec![ConversionType::HeicToJpeg, ConversionType::HeicToPng, ConversionType::HeicToWebp],
                 _ => vec![],
             };
             if !self.available_conversions.is_empty() {
@@ -108,9 +101,6 @@ impl eframe::App for MyApp {
                         Some(ConversionType::PdfToJpeg) => "PDF to JPEG",
                         Some(ConversionType::PdfToPng) => "PDF to PNG",
                         Some(ConversionType::PdfToWebp) => "PDF to WebP",
-                        Some(ConversionType::HeicToJpeg) => "jpeg",
-                        Some(ConversionType::HeicToPng) => "png",
-                        Some(ConversionType::HeicToWebp) => "webp",
                         None => "Select Conversion",
                     })
                     .show_ui(ui, |ui| {
@@ -125,9 +115,6 @@ impl eframe::App for MyApp {
                                 ConversionType::PdfToJpeg => "PDF to JPEG",
                                 ConversionType::PdfToPng => "PDF to PNG",
                                 ConversionType::PdfToWebp => "PDF to WebP",
-                                ConversionType::HeicToJpeg => "HEIC to JPEG",
-                                ConversionType::HeicToPng => "HEIC to PNG",
-                                ConversionType::HeicToWebp => "HEIC to WebP",
                             });
                         }
                     });
@@ -182,9 +169,6 @@ impl eframe::App for MyApp {
                         Some(ConversionType::PdfToJpeg) => "jpeg",
                         Some(ConversionType::PdfToPng) => "png",
                         Some(ConversionType::PdfToWebp) => "webp",
-                        Some(ConversionType::HeicToJpeg) => "jpeg",
-                        Some(ConversionType::HeicToPng) => "png",
-                        Some(ConversionType::HeicToWebp) => "webp",
                         _ => "",
                     };
     
@@ -205,9 +189,6 @@ impl eframe::App for MyApp {
                         Some(ConversionType::PdfToJpeg) => convert_pdf_to_image(&self.input_path, final_output_path.to_str().unwrap(), "jpeg"),
                         Some(ConversionType::PdfToPng) => convert_pdf_to_image(&self.input_path, final_output_path.to_str().unwrap(), "png"),
                         Some(ConversionType::PdfToWebp) => convert_pdf_to_image(&self.input_path, final_output_path.to_str().unwrap(), "webp"),
-                        Some(ConversionType::HeicToJpeg) => convert_heic_to_jpeg(&self.input_path, final_output_path.to_str().unwrap()), // New handler
-                        Some(ConversionType::HeicToPng) => convert_heic_to_png(&self.input_path, final_output_path.to_str().unwrap()),   // New handler
-                        Some(ConversionType::HeicToWebp) => convert_heic_to_webp(&self.input_path, final_output_path.to_str().unwrap()), // New handler
                         _ => Err("Unsupported conversion type".into()),
                     };
     
